8429f02cffd24d8927b0f2f3e610ec8d275e69a2
Omar Ashraf <eng.omar.ashraf26@gmail.com>
2024-07-30 22:16:15 +0300

    Updated Signup

--- client/src/app/shared/components/header/header.component.ts
+++ client/src/app/shared/components/header/header.component.ts
@@ -5,7 +5,4 @@ import { Component } from '@angular/core';
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
 })
-export class HeaderComponent {
-  login() {}
-  signup() {}
-}
+export class HeaderComponent {}
